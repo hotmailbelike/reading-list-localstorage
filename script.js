bef358df29d057a8b0febed772580ec3d14abc88
hotmailbelike <fishilike.fish@gmail.com>
2019-10-16 09:08:14 +0600

    CW 6

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-let books = [] // array containing book objects {title: '', author: ''}
+let books = []; // array containing book objects {title: '', author: ''}
 
 /* Check localStorage for books */
-
-/* Check if books is empty and show/hide relevant message */ 
+if (window.localStorage.getItem("bookRow")) {
+  books = JSON.parse(window.localStorage.getItem("bookRow"));
+}
+/* Check if books is empty and show/hide relevant message */
 
 /* If books is not emply, populate view with book details in #list */
 
-/* Add a book button click should reveal the form */
+if (books.length != 0) {
+  $("#empty").hide();
+  books.forEach(e => {
+    let item = $("<li>").text(e.title + " - " + e.author);
+    $("#list").append(item);
+  });
+}
 
+/* Add a book button click should reveal the form */
+$("#show-add").click(e => {
+  $("#addForm").show();
+  $(this).hide();
+});
 /* On ADD button click, 
     ensure the input fields contain values, 
     then add the book to HTML view 
     also save updated books array to localStorage 
 */
+
+$("#add").click(e => {
+  var title = $("#title").val();
+  var author = $("#author").val();
+  if (title && author) {
+    let item = $("<li>").text(title + " - " + author);
+    $("#list").append(item);
+    books.push({ title: title, author: author });
+    window.localStorage.setItem("bookRow", JSON.stringify(books));
+  } else {
+    alert("Fields Empty!!");
+    console.log("Fields Empty!!");
+  }
+});
 
 /* Refreshing the page, should show all the stored books */
